3a4e5a4f0e745a322572916ad1c8ebc4dfae27ce
sukumar <sukumarg.7310@gmail.com>
2025-08-02 20:31:48 +0530

    ec2 instances and route53 through shell

--- roboshop-shell.sh
+++ roboshop-shell.sh
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-05ffe3c48a9991133 --instance-type $INSTANCE_TYPE --security-group-ids sg-050ef4889bc4a5a82 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query Instances[0].PrivateIpAddress --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-05ffe3c48a9991133 --instance-type $INSTANCE_TYPE --security-group-ids sg-050ef4889bc4a5a82 --subnet-id subnet-0bfd3c097b97c41a5 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query Instances[0].PrivateIpAddress --output text)
     echo "$i: $IP_ADDRESS"
 
      #create R53 record, make sure you delete existing record
